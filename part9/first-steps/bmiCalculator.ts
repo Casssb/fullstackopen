bec53f3a8591490e2489054f454310a973d893d2
Joe <caspar0912@googlemail.com>
2025-07-27 12:56:04 +0100

    part9 first steps

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,50 @@
+type BMICategory =
+  | 'Underweight'
+  | 'Healthy Weight'
+  | 'Overweight'
+  | 'Obese'
+  | 'Hippo';
+
+const processBMIArguments = (args: string[]) => {
+  if (args.length < 4)
+    throw new Error('Not enough arguments (two numbers required)');
+  if (args.length > 4)
+    throw new Error('Too many arguments (two numbers required)');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      heightCM: Number(args[2]),
+      weightKG: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+const calculateBMI = (heightCM: number, weightKG: number): BMICategory => {
+  const heightMeters = heightCM / 100;
+  const bmiCalculation = weightKG / heightMeters ** 2;
+
+  if (bmiCalculation > 40) {
+    return 'Hippo';
+  } else if (bmiCalculation > 30) {
+    return 'Obese';
+  } else if (bmiCalculation > 25) {
+    return 'Overweight';
+  } else if (bmiCalculation > 18.5) {
+    return 'Healthy Weight';
+  } else {
+    return 'Underweight';
+  }
+};
+
+try {
+  const { heightCM, weightKG } = processBMIArguments(process.argv);
+  console.log(calculateBMI(heightCM, weightKG));
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log('An unkown error occured');
+  }
+}
---
+++ part9/first-steps/exerciseCalculator.ts
@@ -0,0 +1,101 @@
+const ratings = {
+  1: 'not good',
+  2: 'ok',
+  3: 'good',
+} as const;
+
+type Rating = keyof typeof ratings;
+type RatingDescription = (typeof ratings)[Rating];
+
+interface ExerciseReport {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: Rating;
+  ratingDescription: RatingDescription;
+  target: number;
+  average: number;
+}
+
+interface RatingOutcome {
+  rating: Rating;
+  ratingDescription: RatingDescription;
+}
+
+const processExcersieCalculatorArguments = (args: string[]) => {
+  if (args.length < 4)
+    throw new Error('Not enough arguments (two numbers required)');
+
+  const formattedNumbers = args.slice(2).map((arg) => {
+    if (!isNaN(Number(arg))) {
+      return Number(arg);
+    } else {
+      throw new Error(`Provided value ${arg} is not a number!`);
+    }
+  });
+
+  return {
+    targetHours: formattedNumbers[0],
+    dailyExerciseHours: formattedNumbers.slice(1),
+  };
+};
+
+const calculateRating = (
+  averageHours: number,
+  targetHours: number
+): RatingOutcome => {
+  const result = targetHours - averageHours;
+  if (result < 0) {
+    return {
+      rating: 3,
+      ratingDescription: 'good',
+    };
+  } else if (result < 0.5) {
+    return {
+      rating: 2,
+      ratingDescription: 'ok',
+    };
+  } else {
+    return {
+      rating: 1,
+      ratingDescription: 'not good',
+    };
+  }
+};
+
+const calculateExercises = (
+  dailyExerciseHours: number[],
+  targetHours: number
+): ExerciseReport => {
+  const periodLength = dailyExerciseHours.length;
+  const totalDaysExcercised = dailyExerciseHours.filter(Boolean).length;
+  const sumOfDailyHours = dailyExerciseHours.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
+  const averageHoursExercised = sumOfDailyHours / periodLength;
+
+  const ratingOutcome = calculateRating(averageHoursExercised, targetHours);
+
+  return {
+    periodLength: periodLength,
+    trainingDays: totalDaysExcercised,
+    success: totalDaysExcercised === periodLength,
+    rating: ratingOutcome.rating,
+    ratingDescription: ratingOutcome.ratingDescription,
+    target: targetHours,
+    average: parseFloat(averageHoursExercised.toFixed(2)),
+  };
+};
+
+try {
+  const { targetHours, dailyExerciseHours } =
+    processExcersieCalculatorArguments(process.argv);
+  console.log(calculateExercises(dailyExerciseHours, targetHours));
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log('An unkown error occured');
+  }
+}
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
